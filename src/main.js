d8fa34124b96954a0d3f1d82e052f1188e562dcd
moli <136324426+lostf1sh@users.noreply.github.com>
2024-07-04 23:45:52 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App.vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faStar, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin, faDiscord, faSpotify, faMastodon, faInstagram} from '@fortawesome/free-brands-svg-icons';
+import { faStar, faCodeBranch, faCode } from '@fortawesome/free-solid-svg-icons'; // Add faCode here
+import { faGithub, faLinkedin, faDiscord, faSpotify, faMastodon, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-// Add Instagram to the library
-library.add(faGithub, faLinkedin, faDiscord, faSpotify, faMastodon, faStar, faCodeBranch, faInstagram);
+// Add icons to the library
+library.add(faGithub, faLinkedin, faDiscord, faSpotify, faMastodon, faInstagram, faStar, faCodeBranch, faCode);
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
