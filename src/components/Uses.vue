aea25a2be490abb7ddcc2e5c1091e58dddca0cf4
lareii <lareii@proton.me>
2024-08-30 22:36:44 +0300

    refactor: update used techs icons

--- src/components/Uses.vue
+++ src/components/Uses.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="mb-2 font-black text-2xl">uses/</div>
-  <img src="https://skillicons.dev/icons?i=linux,git,docker,vscode,discord,figma,github,mongodb" class="select-none mb-2" />
-  <img src="https://skillicons.dev/icons?i=c,python,html,css,tailwind,javascript,vue,django,flask" class="select-none" />
+  <img src="https://skillicons.dev/icons?i=linux,git,docker,vscode,github,mongodb" class="select-none mb-2" />
+  <img src="https://skillicons.dev/icons?i=c,python,go,js,tailwind,react,vuejs,nextjs" class="select-none" />
 </template>
